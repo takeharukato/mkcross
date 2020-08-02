7d1572889ba0c1ad6301cf2ea1307235f1ba919e
Takeharu KATO <tkato@elliptic-curve.net>
2020-08-02 19:36:21 +0900

    修正概要: RHEL8/CentOS8 dnfコマンド対応。Python3対応。

--- cross-llvm/env/llvm-env.sh
+++ cross-llvm/env/llvm-env.sh
@@ -1,13 +1,13 @@
+###############################################################################
 # -*- mode: shell; coding:utf-8 -*-
 ###############################################################################
-###############################################################################
-PYTHON_VER=python2.7
-CMAKE=cmake-3.15.5
+PYTHON_VER=python3.6
+CMAKE=cmake-3.18.1
 Z3=z3-4.8.4
-QEMU=qemu-3.1.0
+QEMU=qemu-5.0.0
 NINJA_URL=https://github.com/martine/ninja.git
-DOWNLOAD_URLS="https://cmake.org/files/v3.15/cmake-3.15.5.tar.gz
+DOWNLOAD_URLS="https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1.tar.gz
 https://github.com/Z3Prover/z3/archive/z3-4.8.4.tar.gz
-https://download.qemu.org/qemu-3.1.0.tar.xz
+https://download.qemu.org/qemu-5.0.0.tar.xz
 https://opensource.apple.com/source/lldb/lldb-69/utils/emacs/gud.el"
 
